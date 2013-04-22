2ff4978efcf1fd2609112a644ae305620d2c9de0
Yuri K. Schlesner <yuriks@yuriks.net>
2013-04-22 02:48:06 -0500

    Removed "debug sprite" code.

--- src/render/debug_sprite.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct SpriteBufferIndices;
-
-void debugPoint(int x, int y);
-
-void initDebugSprites();
-void deinitDebugSprites();
-void drawDebugSprites(SpriteBufferIndices& indices);
